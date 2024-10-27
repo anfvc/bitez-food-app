6859305d358ea62c6cb25d808722d82856edaa00
andresvillay <andres.villay@dci-student.org>
2024-10-27 10:11:39 +0100

    multer ready for prod

--- Backend/Middleware/multer.js
+++ Backend/Middleware/multer.js
@@ -2,38 +2,38 @@ import multer from "multer";
 //Image storage Engine:
 
 //* Code for production:
-// let storage;
+let storage;
 
-// if (process.env.NODE_ENV === "DEVELOPMENT") {
-//   storage = multer.diskStorage({
-//     destination: (req, file, callback) => {
-//       callback(null, "client/public");
-//     },
-//     filename: (req, file, callback) => {
-//       return callback(null, `${Date.now()} ${file.originalname}`); //Filename is unique with Date.now()
-//     },
-//     limits: { fileSize: 150000 }, // 150kb
-//   });
-// } else {
-//   storage = multer.diskStorage({
-//     destination: (req, file, callback) => {
-//       callback(null, "client/dist");
-//     },
-//     filename: (req, file, callback) => {
-//       return callback(null, `${Date.now()} ${file.originalname}`); //Filename is unique with Date.now()
-//     },
-//     limits: { fileSize: 150000 }, // 150kb
-//   });
-// }
+if (process.env.NODE_ENV === "DEVELOPMENT") {
+  storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+      callback(null, "client/public");
+    },
+    filename: (req, file, callback) => {
+      return callback(null, `${Date.now()} ${file.originalname}`); //Filename is unique with Date.now()
+    },
+    limits: { fileSize: 150000 }, // 150kb
+  });
+} else {
+  storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+      callback(null, "client/dist");
+    },
+    filename: (req, file, callback) => {
+      return callback(null, `${Date.now()} ${file.originalname}`); //Filename is unique with Date.now()
+    },
+    limits: { fileSize: 150000 }, // 150kb
+  });
+}
 
 // ? Code for development:
 
-const storage = multer.diskStorage({
-  destination: "Uploads",
-  filename: (req, file, callback) => {
-    return callback(null, `${Date.now()} ${file.originalname}`); //Filename is unique with Date.now()
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: "Uploads",
+//   filename: (req, file, callback) => {
+//     return callback(null, `${Date.now()} ${file.originalname}`); //Filename is unique with Date.now()
+//   },
+// });
 
 const upload = multer({ storage: storage });
 
