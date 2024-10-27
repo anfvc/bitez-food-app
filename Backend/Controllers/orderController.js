ad313e1560e0461874591d102e9f4f9de9f904fc
andresvillay <andres.villay@dci-student.org>
2024-10-27 13:46:49 +0100

    adjusting verifyOrder

--- Backend/Controllers/orderController.js
+++ Backend/Controllers/orderController.js
@@ -69,7 +69,7 @@ export async function verifyOder(req, res, next) {
   const { orderId, success } = req.body;
 
   try {
-    if (success === "true") {
+    if (success == "true") {
       await Order.findByIdAndUpdate(orderId, { payment: true });
       res.status(200).json({
         message: `Order ${orderId} has been successfully paid.`,
